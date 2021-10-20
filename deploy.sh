433e13f5c7733fe66239c7e9310d6225b7dcad41
Ana Lallena Arquillo <lallena.ana@gmail.com>
2021-10-20 18:51:25 +0200

    adding deployment

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
-docker build -t analallena/multi-client:latest -t analallena/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t analallena/multi-worker:latest -t analallena/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker build -t analallena/muti-server:latest -t analallena/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t analallena/multi-client-k8s:latest -t analallena/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t analallena/multi-worker-k8s:latest -t analallena/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t analallena/muti-server-k8s:latest -t analallena/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
 
-docker push analallena/multi-client:latest
-docker push analallena/multi-server:latest
-docker push analallena/multi-worker:latest
+docker push analallena/multi-client-k8s:latest
+docker push analallena/multi-server-k8s:latest
+docker push analallena/multi-worker-k8s:latest
 
-docker push analallena/multi-client:$SHA
-docker push analallena/multi-server:$SHA
-docker push analallena/multi-worker:$SHA
+docker push analallena/multi-client-k8s:$SHA
+docker push analallena/multi-server-k8s:$SHA
+docker push analallena/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/server-deployment server=analallena/multi-server:$SHA
-kubectl set image deployments/client-deployment client=analallena/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=analallena/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=analallena/multi-server-k8s:$SHA
+kubectl set image deployments/client-deployment client=analallena/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=analallena/multi-worker-k8s:$SHA
 
